d0838a771ab36489bf62c85aa8b4d1be61a11685
Rama Sabajo <rsabajo@gmail.com>
2020-11-04 23:42:54 +0100

    gras toegevoegd en guesses state gemaakt

--- src/components/GameState.js
+++ src/components/GameState.js
@@ -1,21 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import balk1 from '../images/balk1.png';
 import balk2 from '../images/balk2.png';
 import steunbalk from '../images/steunbalk.png';
 import touw from '../images/touw.png';
 import popje from '../images/popje.png';
+import gras from '../images/gras.png';
 
 export default function GameState() {
+  const [guesses, setGuesses] = useState(5);
+
   return (
     <div className="gameState">
       <div className="guesses">
         <p>
-          je mag nog <span className="guesses_left">5</span> letters raden
+          je mag nog <span className="guesses_left">{guesses}</span> keer raden
         </p>
       </div>
 
       <div className="galgjeImages">
         <div className="galgje">
+          <img src={gras} alt="" className="gras" />
+          <img src={gras} alt="" className="gras2" />
           <img src={balk1} alt="" className="balk1" />
           <img src={balk2} alt="" className="balk2" />
           <img src={steunbalk} alt="" className="steunbalk" />
