e7bbc4b9f55ab15e314495adc530d278678e77bb
Rama Sabajo <rsabajo@gmail.com>
2020-11-10 23:23:06 +0100

    added dancing man image

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import dancing from '../images/dancing.gif';
 
 const Popup = ({
   correctLetters,
@@ -10,6 +11,7 @@ const Popup = ({
   let finalMessage = '';
   let selectedWordArray = selectedWord.split('');
   let playable = true;
+  let img = '';
 
   // function to check if correctLetters array includes all of the letters in the selected Word.
   let checker = (arr, target) => target.every(l => arr.includes(l));
@@ -17,6 +19,9 @@ const Popup = ({
   if (checker(correctLetters, selectedWordArray)) {
     finalMessage = `Gefeliciteerd! \n Je hebt gewonnen! \n 🎉🥳🙌🎊`;
     playable = false;
+    img = (
+      <img className="victoryDance" src={dancing} alt="victory dancing man" />
+    );
   } else if (wrongLetters.length === 6) {
     finalMessage = `Helaas... je hebt verloren. 😢 \n het woord dat ik zocht was \n ${selectedWord}`;
     playable = false;
@@ -32,6 +37,7 @@ const Popup = ({
       <div className="popup">
         <h2>{finalMessage}</h2>
         <button onClick={playAgain}>Opnieuw Spelen</button>
+        {img}
       </div>
     </div>
   );
